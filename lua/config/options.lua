31fde5dbacf539b6c9ae6cfcfaa45e858b0f6391
JulianDPastrana <j.pastrana@utp.edu.co>
2025-05-07 20:46:27 -0500

    First commit

---
+++ lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- force system clipboard always
+local opt = vim.opt
+opt.clipboard = "unnamedplus"
