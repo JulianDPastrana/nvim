130581312d90c5bed4b74f48579bec4decfaf6b9
JulianDPastrana <j.pastrana@utp.edu.co>
2026-07-15 17:24:26 -0500

    Initial nvim config: LazyVim + catppuccin macchiato transparent theme

---
+++ lua/plugins/core.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "macchiato",
+      transparent_background = true,
+    },
+  },
+}
